fb8c8d0ce7505e4d233a89b2e79fc11c03a320fc
MBUVI DAVID <147725809+cheekydavy@users.noreply.github.com>
2025-04-01 12:34:56 +0300

    Update and rename fbscamblaster_cli.py to fbscamblaster.py

---
+++ fbscamblaster.py
@@ -0,0 +1,183 @@
+# fb_report_tool.py
+import requests 
+import time 
+import colorama
+from colorama import Fore, Back ,Style
+import os
+
+os.system("clear")
+time.sleep(1)
+
+print (Fore.GREEN + "report tool started ...")
+time.sleep(1)
+
+os.system("clear")
+
+slaw0= '''
+________________________________________________________________________________
+'''
+print(Fore.YELLOW + slaw0)
+
+slaw = '''
+	 ____________________________________________________________
+	| CCCCC H    H EEEEEEE EEEEEEE K   K YYYYYYY                |
+	|  CC     H    H E       E       K  K      Y                 |
+	| C        HHHHHH EEEEE   EEEEE   KK       Y                 |
+	|  CC     H    H E       E       K  K      Y                 |
+	|   CCCCC H    H EEEEEEE EEEEEEE K   K      Y                |
+	|____________________________________________________________|
+	|                  Created by cheekydavy                     |
+	|____________________________________________________________|
+'''
+print(Fore.RED + slaw)
+
+slaw2 = '''
+		* * * * * * * * * * * * * * * * * * * * *
+		* [#]  welcome to facebook report tool  *
+		*                                       *
+		* [#]  created : cheekydavy             * 
+		*                                       *
+		* [#] try to use it for ethical purpose *
+		* * * * * * * * * * * * * * * * * * * * *
+	'''
+print(Fore.GREEN + slaw2)
+	
+slaw3 = '''
+__________________________________________________________________________________
+	'''
+print(Fore.BLUE + slaw3)
+
+#00000000000000000000000000000000000000000000000000   functionakan    0000000000000000000000000000000000000000000000000000000000
+
+
+fb =input(Fore.YELLOW + '[+] Please inter the facebook URL : ')
+time.sleep(2)
+
+os.system("clear")
+
+print("report automation is startig.....")
+
+url = 'https://m.facebook.com/help/contact/209046679279097'
+
+data = {
+'crt-url' : fb,
+'cf_age':"less than 9 years",
+'submit':'submit'
+}
+whisper=requests.post(url,data=data)
+print(Fore.RED + '[+]report done....')
+
+data = {
+'crt-url' : fb,
+'cf_age':"less than 9 years",
+'submit':'submit'
+}
+whisper=requests.post(url,data=data)
+print('[+]report done....')
+
+data = {
+'crt-url' : fb,
+'cf_age':"less than 9 years",
+'submit':'submit'
+}
+whisper=requests.post(url,data=data)
+print('[+]report done....')
+
+data = {
+'crt-url' : fb,
+'cf_age':"less than 9 years",
+'submit':'submit'
+}
+whisper=requests.post(url,data=data)
+print('[+]report done....')
+
+data = {
+'crt-url' : fb,
+'cf_age':"less than 9 years",
+'submit':'submit'
+}
+whisper=requests.post(url,data=data)
+print('[+]report done....')
+
+data = {
+'crt-url' : fb,
+'cf_age':"less than 9 years",
+'submit':'submit'
+}
+whisper=requests.post(url,data=data)
+print('[+]report done....')
+
+data = {
+'crt-url' : fb,
+'cf_age':"less than 9 years",
+'submit':'submit'
+}
+whisper=requests.post(url,data=data)
+print('[+]report done....')
+
+data = {
+'crt-url' : fb,
+'cf_age':"less than 9 years",
+'submit':'submit'
+}
+whisper=requests.post(url,data=data)
+print('[+]report done....')
+
+data = {
+'crt-url' : fb,
+'cf_age':"less than 9 years",
+'submit':'submit'
+}
+whisper=requests.post(url,data=data)
+print('[+]report done....')
+
+data = {
+'crt-url' : fb,
+'cf_age':"less than 9 years",
+'submit':'submit'
+}
+whisper=requests.post(url,data=data)
+print('[+]report done....')
+
+data = {
+'crt-url' : fb,
+'cf_age':"less than 9 years",
+'submit':'submit'
+}
+whisper=requests.post(url,data=data)
+print('[+]report done....')
+
+data = {
+'crt-url' : fb,
+'cf_age':"less than 9 years",
+'submit':'submit'
+}
+whisper=requests.post(url,data=data)
+print('[+]report done....')
+
+data = {
+'crt-url' : fb,
+'cf_age':"less than 9 years",
+'submit':'submit'
+}
+whisper=requests.post(url,data=data)
+print('[+]report done....')
+
+data = {
+'crt-url' : fb,
+'cf_age':"less than 9 years",
+'submit':'submit'
+}
+whisper=requests.post(url,data=data)
+print('[+]report done....')
+
+data = {
+'crt-url' : fb,
+'cf_age':"less than 9 years",
+'submit':'submit'
+}
+whisper=requests.post(url,data=data)
+print('[+]report done....')
+time.sleep(2)
+
+print (Fore.BLUE + "successfully we reported the facebook URL ...")
